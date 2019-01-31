04770c1331b98ca59182de48c65f8153aede7773
Pablo Vila <pablo.vilafer@gmail.com>
2019-01-31 21:54:17 +0100

    added new intent for next event

--- src/index.js
+++ src/index.js
@@ -1,34 +1,36 @@
 const Alexa = require("ask-sdk-core");
 
 const {
-    HelpHandler,
-    ExitHandler, 
-    LaunchRequestHandler, 
-    SessionEndedRequestHandler} = require("./intents/defaults");
+  HelpHandler,
+  ExitHandler,
+  LaunchRequestHandler,
+  SessionEndedRequestHandler
+} = require("./intents/defaults");
 const errorHandler = require("./intents/error");
 
 const SaludarTodosHandler = require("./intents/SaludarTodosHandler");
 const SaludarAlguienHandler = require("./intents/SaludarAlguienHandler");
+const ProximoEventoHandler = require("./intents/ProximoEventoHandler");
 const DespedirHandler = require("./intents/DespedirHandler");
 
 const skillBuilder = Alexa.SkillBuilders.custom();
 
 exports.handler = async (event, context) => {
-    
-    const skill = skillBuilder
-        .addRequestHandlers(
-            ExitHandler,
-            HelpHandler,
-            LaunchRequestHandler,
-            SessionEndedRequestHandler
-        )
-        .addRequestHandlers(
-            SaludarTodosHandler,
-            SaludarAlguienHandler,
-            DespedirHandler
-        )
-        .addErrorHandlers(errorHandler)
-        .create();
+  const skill = skillBuilder
+    .addRequestHandlers(
+      ExitHandler,
+      HelpHandler,
+      LaunchRequestHandler,
+      SessionEndedRequestHandler
+    )
+    .addRequestHandlers(
+      SaludarTodosHandler,
+      SaludarAlguienHandler,
+      ProximoEventoHandler,
+      DespedirHandler
+    )
+    .addErrorHandlers(errorHandler)
+    .create();
 
-    return await skill.invoke(event, context);
-}
+  return await skill.invoke(event, context);
+};
